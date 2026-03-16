08bf3f0ecf7eee03f85ada9fdd17a8860b64dd39
Kelly Farran <klfarran@comcast.net>
2026-03-15 20:05:16 -0400

    Added PCIe bandwidth info

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -16,7 +16,8 @@ echo "--------------------------------------------"
 for b in "${BENCHES[@]}"; do
     echo ""
     echo "Running $b..."
-    ./$b
+    run_output=$(./$b)
+    echo "$run_output"
 
     nsys profile \
         --trace=cuda,nvtx,osrt \
@@ -30,15 +31,46 @@ for b in "${BENCHES[@]}"; do
 
     nsys stats ${b}_profile.nsys-rep > /dev/null
 
-    echo "Page Fault Stats:"
+    echo "Page Fault and Migration Information:"
+    echo "--------------------------------------------"
     output=$(nsys stats --quiet -r um_total_sum ${b}_profile.sqlite 2>/dev/null)
 
     if [ -z "$output" ]; then
         echo "No Unified Memory page faults occurred"
     else
-        echo "$output"
+        numbers_line=$(echo "$output" | tail -n 1)
+        nums=($(grep -Eo '[0-9]+(\.[0-9]+)?' <<< "$numbers_line"))
+        count=${#nums[@]}
+        
+        if [ "$count" -eq 4 ]; then
+            htod_mb=${nums[0]}
+            dtoh_mb=${nums[1]}
+            cpu_pfs=${nums[2]}
+            gpu_pfs=${nums[3]}
+        elif [ "$count" -eq 3 ]; then
+            htod_mb=${nums[0]}
+            dtoh_mb=0
+            cpu_pfs=${nums[1]}
+            gpu_pfs=${nums[2]}
+        fi
+
+        echo "CPU Page Faults: $cpu_pfs"
+        echo "GPU Page Faults: $gpu_pfs"
+    
+        echo "HtoD migration size: $htod_mb MB"
+        echo "DtoH migration size: $dtoh_mb MB"
+
+        kernel_time=$(echo "$run_output" | grep "Kernel Time" | awk '{print $(NF-1)}')
+
+        htod_bandwidth=$(echo "$htod_mb / 1024 / ($kernel_time / 1000)" | bc -l)
+        dtoh_bandwidth=$(echo "$dtoh_mb / 1024 / ($kernel_time / 1000)" | bc -l)
+        
+        printf "Approx PCIe Bandwidth for HtoD Migrations: %.2f GB/s\n" "$htod_bandwidth"
+        printf "Approx PCIe Bandwidth for DtoH Migrations: %.2f GB/s\n" "$dtoh_bandwidth"
+
     fi
 
 echo ""
 
 done
+
